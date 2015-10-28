53f0e406147515c36f53efe5bfdf8735ca00029e
z80 <muzo0@yandex.ru>
2015-10-28 16:06:29 +0300

    add font-awesome,url-loader,font-loader

--- build/webpack/_base.js
+++ build/webpack/_base.js
@@ -79,7 +79,9 @@ const webpackConfig = {
           'file?hash=sha512&digest=hex&name=[hash].[ext]',
           'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
         ]
-      }
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
   eslint : {
