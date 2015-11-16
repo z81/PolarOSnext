64c100ab6eabbca48749a8ce5b8a95b92dcb5c4d
z80 <muzo0@yandex.ru>
2015-11-16 15:46:16 +0300

    add login view

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import { Route }   from 'react-router';
 import React       from 'react';
 import CoreLayout  from 'layouts/CoreLayout';
 import HomeView    from 'views/HomeView';
+import LoginView   from 'views/LoginView';
 
 export default (
   <Route component={CoreLayout}>
     <Route name='home' path='/' component={HomeView} />
+    <Route name='login' path='/login' component={LoginView} />
   </Route>
 );
